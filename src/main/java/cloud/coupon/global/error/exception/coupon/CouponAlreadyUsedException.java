1250103b1fdd17c34f297c271d57fdcc5b91fe8f
Byuntil <gugugu2014@jbnu.ac.kr>
2024-11-22 23:53:32 +0900

    feat: 쿠폰 사용 불가능 예외, 이미 사용한 쿠폰 예외 추가

---
+++ src/main/java/cloud/coupon/global/error/exception/coupon/CouponAlreadyUsedException.java
@@ -0,0 +1,7 @@
+package cloud.coupon.global.error.exception.coupon;
+
+public class CouponAlreadyUsedException extends CouponException {
+    public CouponAlreadyUsedException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/cloud/coupon/global/error/exception/coupon/CouponNotAvailableException.java
@@ -0,0 +1,7 @@
+package cloud.coupon.global.error.exception.coupon;
+
+public class CouponNotAvailableException extends CouponException {
+    public CouponNotAvailableException(String message) {
+        super(message);
+    }
+}
