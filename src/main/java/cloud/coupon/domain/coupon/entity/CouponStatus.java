cba69975b381552b08b18feac82b9eea4d73bde7
Byuntil <gugugu2014@jbnu.ac.kr>
2024-11-22 23:48:22 +0900

    refactor(CouponStatus): 쿠폰 상태 변경

--- src/main/java/cloud/coupon/domain/coupon/entity/CouponStatus.java
+++ src/main/java/cloud/coupon/domain/coupon/entity/CouponStatus.java
@@ -1,10 +1,10 @@
 package cloud.coupon.domain.coupon.entity;
 
 public enum CouponStatus {
-    READY("발급 대기"),
-    ACTIVE("발급 중"),
+    ACTIVE("활성 상태"),
     EXHAUSTED("소진됨"),
-    STOPPED("발급 중단");
+    EXPIRED("만료됨"),
+    DISABLED("비활성화됨");
 
     private String description;
 
