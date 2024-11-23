95779e036fd80d9c5e8ca85a7a4cd14827588c31
Byuntil <gugugu2014@jbnu.ac.kr>
2024-11-23 19:26:52 +0900

    feat: 이미 삭제된 쿠폰 예외 추가

---
+++ src/main/java/cloud/coupon/global/error/exception/coupon/CouponAlreadyDeletedException.java
@@ -0,0 +1,7 @@
+package cloud.coupon.global.error.exception.coupon;
+
+public class CouponAlreadyDeletedException extends CouponException {
+    public CouponAlreadyDeletedException(String message) {
+        super(message);
+    }
+}
