c82c9219525e107be46f9881522da6ef4de945d3
Byuntil <gugugu2014@jbnu.ac.kr>
2024-11-27 00:18:53 +0900

    fix(CouponService): 쿠폰 사용시 쿠폰 사용 저장소에 저장 로직 추가

--- src/main/java/cloud/coupon/domain/coupon/service/CouponService.java
+++ src/main/java/cloud/coupon/domain/coupon/service/CouponService.java
@@ -6,6 +6,7 @@ import static cloud.coupon.domain.coupon.constant.ErrorMessage.COUPON_NOT_FOUND_
 
 import cloud.coupon.domain.coupon.dto.request.CouponIssueRequest;
 import cloud.coupon.domain.coupon.dto.response.CouponIssueResult;
+import cloud.coupon.domain.coupon.dto.response.CouponUseResponse;
 import cloud.coupon.domain.coupon.entity.Coupon;
 import cloud.coupon.domain.coupon.entity.CouponIssue;
 import cloud.coupon.domain.coupon.entity.IssueResult;
@@ -13,7 +14,9 @@ import cloud.coupon.domain.coupon.repository.CouponIssueRepository;
 import cloud.coupon.domain.coupon.repository.CouponRepository;
 import cloud.coupon.domain.coupon.util.CodeGenerator;
 import cloud.coupon.domain.history.entity.CouponIssueHistory;
+import cloud.coupon.domain.history.entity.CouponUseHistory;
 import cloud.coupon.domain.history.repository.CouponIssueHistoryRepository;
+import cloud.coupon.domain.history.repository.CouponUseHistoryRepository;
 import cloud.coupon.global.error.exception.coupon.CouponNotFoundException;
 import cloud.coupon.global.error.exception.coupon.DuplicateCouponException;
 import cloud.coupon.global.error.exception.couponissue.CouponIssueNotFoundException;
@@ -28,6 +31,7 @@ public class CouponService {
     private final CouponRepository couponRepository;
     private final CouponIssueRepository couponIssueRepository;
     private final CouponIssueHistoryRepository couponIssueHistoryRepository;
+    private final CouponUseHistoryRepository couponUseHistoryRepository;
     private final CodeGenerator couponCodeGenerator;
 
     // 쿠폰 발급
@@ -53,12 +57,21 @@ public class CouponService {
 
     // 쿠폰 사용 처리 - 데이터 수정 필요
     @Transactional
-    public void useCoupon(String issueCode, Long userId) {
+    public CouponUseResponse useCoupon(Long userId, String issueCode) {
         // 사용 처리 로직
         CouponIssue couponIssue = couponIssueRepository.findByIssuedCodeAndUserId(issueCode, userId)
                 .orElseThrow(() -> new CouponIssueNotFoundException(COUPON_ISSUE_NOT_FOUND_MESSAGE));
 
         couponIssue.use();
+
+        CouponUseHistory useHistory = createCouponUseHistory(userId, couponIssue);
+        couponUseHistoryRepository.save(useHistory);
+
+        return new CouponUseResponse(
+                true,
+                useHistory.getDiscountValue(),
+                useHistory.getUsedAt()
+        );
     }
 
     private void validateDuplicateIssue(String code, Long userId) {
@@ -89,5 +102,15 @@ public class CouponService {
                         .failReason(failReason)
                         .build()
         );
+    }
+
+    private CouponUseHistory createCouponUseHistory(Long userId, CouponIssue couponIssue) {
+        return CouponUseHistory.builder()
+                .couponIssue(couponIssue)
+                .userId(userId)
+                .discountType(couponIssue.getCoupon().getType())
+                .discountValue(couponIssue.getCoupon().getDiscountValue())
+                .usedAt(couponIssue.getUsedAt())
+                .build();
     }
 }
