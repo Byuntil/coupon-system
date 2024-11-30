3526a5298e0e395f2599187032c50c0f83c66fb3
Byuntil <gugugu2014@jbnu.ac.kr>
2024-11-30 23:44:01 +0900

    feat(CouponRepository): 비관적 락 적용 + 활성 상태의 쿠폰 모두 조회 기능 구현

--- src/main/java/cloud/coupon/domain/coupon/repository/CouponRepository.java
+++ src/main/java/cloud/coupon/domain/coupon/repository/CouponRepository.java
@@ -1,8 +1,11 @@
 package cloud.coupon.domain.coupon.repository;
 
 import cloud.coupon.domain.coupon.entity.Coupon;
+import jakarta.persistence.LockModeType;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,11 +16,13 @@ import org.springframework.stereotype.Repository;
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
     Optional<Coupon> findByCode(String code);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT c FROM Coupon c WHERE c.code = :code AND c.isDeleted = false")
     Optional<Coupon> findByCodeAndIsDeletedFalse(String code);
 
     @Query("SELECT EXISTS (SELECT 1 FROM Coupon c WHERE c.code = :code AND c.isDeleted = false)")
     boolean existsActiveCodeAndNotDeleted(String code);
-    //재고 업데이트 등
 
+    @Query("SELECT c FROM Coupon c WHERE c.status = 'ACTIVE' AND c.isDeleted = false")
+    List<Coupon> findAllActiveCoupons();
 }
