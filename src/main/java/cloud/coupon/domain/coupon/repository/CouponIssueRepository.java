32ee9c8f38a9e505146da8bc27be8a88ffab9222
Byuntil <gugugu2014@jbnu.ac.kr>
2024-11-30 23:45:32 +0900

    feat(CouponIssueResponse): 비관적 락 적용 + 쿠폰 코드에 해당하는 쿠폰 개수 반환 구현

--- src/main/java/cloud/coupon/domain/coupon/repository/CouponIssueRepository.java
+++ src/main/java/cloud/coupon/domain/coupon/repository/CouponIssueRepository.java
@@ -1,8 +1,10 @@
 package cloud.coupon.domain.coupon.repository;
 
 import cloud.coupon.domain.coupon.entity.CouponIssue;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,5 +14,8 @@ import org.springframework.stereotype.Repository;
 public interface CouponIssueRepository extends JpaRepository<CouponIssue, Long> {
     boolean existsByCouponCodeAndUserId(String code, Long userId); //중복 발급 체크
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<CouponIssue> findByIssuedCodeAndUserId(String code, Long userId); // 발급된 쿠폰 조회
+
+    int countByCouponCode(String code);
 }
