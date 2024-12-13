f49a88fbb69457200198408ec420cc3398cdbc6d
Byuntil <gugugu2014@jbnu.ac.kr>
2024-12-13 22:26:11 +0900

    fix(CouponController): 쿠폰 컨트롤러 예외 처리 추가

--- src/main/java/cloud/coupon/domain/coupon/controller/CouponController.java
+++ src/main/java/cloud/coupon/domain/coupon/controller/CouponController.java
@@ -21,11 +21,17 @@ public class CouponController {
 
     @PostMapping("/issue")
     public ResponseEntity<CouponIssueResult> issueCoupon(@RequestBody CouponIssueRequest request) {
-        CouponIssueResult result = couponService.issueCoupon(request);
-        if (result.isSuccess()) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        try {
+            CouponIssueResult result = couponService.issueCoupon(request);
+
+            if (result.isSuccess()) {
+                return ResponseEntity.status(HttpStatus.CREATED).body(result);
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+        } catch (Exception e) {
+            System.out.println("e.getMessage() = " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
     }
 
     @PostMapping("/use")
