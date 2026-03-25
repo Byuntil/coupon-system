20900551432fe0d2dd6575edc2b08b42be07945a
Byuntil <gugugu2014@jbnu.ac.kr>
2026-03-26 00:37:09 +0900

    feat(GlobalExceptionHandler): 커스텀 예외에 대한 핸들러 추가

--- src/main/java/cloud/coupon/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/cloud/coupon/global/error/handler/GlobalExceptionHandler.java
@@ -1,35 +1,66 @@
 package cloud.coupon.global.error.handler;
 
 import cloud.coupon.global.common.ErrorResponse;
+import cloud.coupon.global.error.exception.coupon.CouponException;
+import cloud.coupon.global.error.exception.coupon.CouponNotFoundException;
+import cloud.coupon.global.error.exception.couponissue.CouponIssueException;
+import cloud.coupon.global.error.exception.couponissue.CouponIssueNotFoundException;
+import cloud.coupon.global.error.exception.redis.RedisException;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult()
                 .getFieldErrors()
                 .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
-        ErrorResponse errorResponse = new ErrorResponse("Validation Error", errors);
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse("Validation Error", errors));
+    }
+
+    @ExceptionHandler(CouponNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCouponNotFoundException(CouponNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse("Coupon Not Found", ex.getMessage()));
+    }
+
+    @ExceptionHandler(CouponIssueNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCouponIssueNotFoundException(CouponIssueNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse("Coupon Issue Not Found", ex.getMessage()));
+    }
+
+    @ExceptionHandler(CouponException.class)
+    public ResponseEntity<ErrorResponse> handleCouponException(CouponException ex) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse("Coupon Error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(CouponIssueException.class)
+    public ResponseEntity<ErrorResponse> handleCouponIssueException(CouponIssueException ex) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse("Coupon Issue Error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(RedisException.class)
+    public ResponseEntity<ErrorResponse> handleRedisException(RedisException ex) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(new ErrorResponse("Service Temporarily Unavailable", ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex) {
-        ErrorResponse errorResponse = new ErrorResponse("Internal Server Error", ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("Internal Server Error", ex.getMessage()));
     }
-
-    //todo 특정 예외를 잡고 처리할 핸들러를 추가
 }
